8a2b1efd54127d0f1a259e56c2afa4517f1ef175
Jacob Bishop <jbishop@ibm.com>
2022-10-22 15:27:43 -0400

    shipments array made

--- src/api/Shipments.js
+++ src/api/Shipments.js
@@ -1,9 +1,128 @@
 export const Shipments = [
-    {origin: "", destination: "",
-     shipmentID:"1", dangerFlag: false, 
-    currentLocation: "city", stops: [], 
-    dateExpected: "datetime", dateDelivered:"datetime" }
+    {
+        dateExpected: 'Thu Mar 16 2023',
+        dateDelivered: 'n/a',
+        dateShipped: 'Tue Jan 24 2023',
+        shipmentID: '1',
+        driverID: '1',
+        origin: 'Texas',
+        destination: 'Bakersfield',
+        dangerFlag: false,
+        currentLocation: 'New York',
+        stops: [ 'Ohio', 'Lubbock', 'New York' ]
+    },
+    {
+        dateExpected: 'Wed Sep 14 2022',
+        dateDelivered: 'Mon Sep 12 2022',
+        dateShipped: 'Wed Aug 10 2022',
+        shipmentID: '2',
+        driverID: '2',
+        origin: 'Indiana',
+        destination: 'Ohio',
+        dangerFlag: false,
+        currentLocation: 'Ohio',
+        stops: [ 'Philadelphia', 'Ohio' ]
+    },
+    {
+        dateExpected: 'Sun Mar 19 2023',
+        dateDelivered: 'Fri Mar 17 2023',
+        dateShipped: 'Wed Feb 08 2023',
+        shipmentID: '3',
+        driverID: '3',
+        origin: 'Indianapolis',
+        destination: 'Memphis',
+        dangerFlag: true,
+        currentLocation: 'Memphis',
+        stops: [ 'Cincinnati', 'Kansas', 'Wichita', 'Nevada', 'Memphis' ]
+    },
+    {
+        dateExpected: 'Fri Jul 29 2022',
+        dateDelivered: 'Tue Jul 26 2022',
+        dateShipped: 'Tue Jul 19 2022',
+        shipmentID: '4',
+        driverID: '4',
+        origin: 'Sacramento',
+        destination: 'Louisiana',
+        dangerFlag: true,
+        currentLocation: 'Louisiana',
+        stops: [ 'Louisiana', 'Texas', 'Louisiana' ]
+    },
+    {
+        dateExpected: 'Wed Jan 25 2023',
+        dateDelivered: 'n/a',
+        dateShipped: 'Wed Jan 04 2023',
+        shipmentID: '5',
+        driverID: '0',
+        origin: 'Lexington',
+        destination: 'Ohio',
+        dangerFlag: false,
+        currentLocation: 'Pittsburgh',
+        stops: [ 'Indianapolis', 'Cincinnati', 'Pittsburgh' ]
+    },
+    {
+        dateExpected: 'Thu Aug 25 2022',
+        dateDelivered: 'n/a',
+        dateShipped: 'Mon Jul 04 2022',
+        shipmentID: '6',
+        driverID: '1',
+        origin: 'Fort Wayne',
+        destination: 'Wichita',
+        dangerFlag: false,
+        currentLocation: 'Nevada',
+        stops: [ 'Henderson', 'Cincinnati', 'Oklahoma City', 'Nevada' ]
+    },
+    {
+        dateExpected: 'Sat Jun 04 2022',
+        dateDelivered: 'Thu Jun 02 2022',
+        dateShipped: 'Wed Apr 13 2022',
+        shipmentID: '7',
+        driverID: '2',
+        origin: 'Sacramento',
+        destination: 'Anchorage',
+        dangerFlag: true,
+        currentLocation: 'Anchorage',
+        stops: [
+        'Buffalo',
+        'Louisiana',
+        'Philadelphia',
+        'North Carolina',
+        'Anchorage'
+        ]
+    },
+    {
+        dateExpected: 'Sat Jul 30 2022',
+        dateDelivered: 'Thu Jul 28 2022',
+        dateShipped: 'Fri Jun 10 2022',
+        shipmentID: '8',
+        driverID: '3',
+        origin: 'El Paso',
+        destination: 'Pennsylvania',
+        dangerFlag: true,
+        currentLocation: 'Pennsylvania',
+        stops: [ 'California', 'Pennsylvania' ]
+    },
+    {
+        dateExpected: 'Mon Oct 02 2023',
+        dateDelivered: 'Fri Sep 29 2023',
+        dateShipped: 'Tue Aug 08 2023',
+        shipmentID: '9',
+        driverID: '4',
+        origin: 'Fort Wayne',
+        destination: 'Garland',
+        dangerFlag: false,
+        currentLocation: 'Garland',
+        stops: [ 'Indiana', 'Bakersfield', 'Garland' ]
+    },
+    {
+        dateExpected: 'Tue May 09 2023',
+        dateDelivered: 'n/a',
+        dateShipped: 'Sun Apr 23 2023',
+        shipmentID: '10',
+        driverID: '0',
+        origin: 'Texas',
+        destination: 'California',
+        dangerFlag: false,
+        currentLocation: 'Kansas',
+        stops: [ 'El Paso', 'Ohio', 'Kansas' ]
+    }
 ]
-
-
-
--- src/api/generate.js
+++ src/api/generate.js
@@ -1,10 +1,104 @@
 import  {Shipments} from "./Shipments.js";
 
+let cities = ['Lexington', 'Fayette',
+'Kentucky','Fort Wayne',
+'Indiana','Philadelphia',
+'Pennsylvania','Reno',
+'Nevada','Garland',
+'Texas','Memphis',
+'Tennessee','Henderson',
+'Nevada','Pittsburgh',
+'Pennsylvania','Wichita',
+'Kansas','Sacramento',
+'California','Lubbock',
+'Texas','Oklahoma City',
+'Oklahoma','Indianapolis',
+'Indiana','Greensboro',
+'North Carolina','Anchorage',
+'Alaska','El Paso',
+'Texas','Cincinnati',
+'Ohio','Chandler',
+'Arizona','New Orleans',
+'Louisiana','Buffalo',
+'New York','Bakersfield',
+'California'];
 
-let startDate = new Date("1/1/2023");
+
 function GenerateDates(){
+    //mm/dd/yyyy
+    let yr = 2021;
+    let smm = Math.floor(Math.random() * (11) + 1);
+    let sdd = Math.floor(Math.random() * (27) + 1);
+    let syyyy = Math.ceil(Math.random() * (2));
+
+    let startDate = new Date(`${smm}/${sdd}/${syyyy + yr}`);
+
+    let dateNum = startDate.getTime();
+
+    let addition =((Math.random() * 50) + 10)*86400000;
     
+    let endDate = new Date(dateNum + addition);
+    
+    let deliverNum = endDate.getTime(); 
+    let m = ((Math.random() * 3) - 4)*86400000;
+    let deliverDate = new Date(deliverNum + m);
+
+    if (Math.random() < 0.3){
+        return [startDate.toDateString(), endDate.toDateString(), "n/a"];
+    }else{
+        return [startDate.toDateString(), endDate.toDateString(), deliverDate.toDateString()]
+    }
+
+
 }
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-console.log(startDate);
+
+let shipArr = [];
+let tempObj = {}
+for (let i = 0; i < 10; i++){
+    tempObj = {};
+    let [shipped, expected, delivered] = GenerateDates();
+    
+    tempObj.dateExpected = expected;
+    tempObj.dateDelivered = delivered;
+    tempObj.dateShipped = shipped;
+    tempObj.shipmentID = `${i + 1}`;
+    tempObj.driverID = `${(i + 1)%5}`;
+    let originIndex = getRandomInt(cities.length - 1);
+    tempObj.origin = cities[originIndex];
+
+    let destIndex = getRandomInt(cities.length - 1);
+    while(destIndex == originIndex){
+        destIndex = getRandomInt(cities.length - 1);
+    }
+    tempObj.destination = cities[destIndex];
+
+    let stops = [];
+    let randStop = 0.0;
+    while(Math.random() > randStop){
+        stops.push(cities[getRandomInt(cities.length - 1)]);
+        randStop += 0.20;
+    }
+
+    if (delivered != "n/a"){
+        stops.push(cities[destIndex]);
+    }
+
+    if (Math.random() > 0.7){
+        tempObj.dangerFlag = true;
+    }else{
+        tempObj.dangerFlag = false;
+    }
+
+    tempObj.currentLocation = stops[stops.length - 1];
+    tempObj.stops = stops;
+
+
+    shipArr.push(tempObj);
+}
+
+console.log(shipArr);

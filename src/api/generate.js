8887f12d65306f0158ad1e258e31b128f2247d43
Jacob Bishop <jbishop@ibm.com>
2022-10-22 18:17:35 -0400

    shipments updated to have packages

--- src/api/generate.js
+++ src/api/generate.js
@@ -1,4 +1,5 @@
 import  {Shipments} from "./Shipments.js";
+import fs from "fs/promises";
 //const fs = require('fs');
 
 let cities = ['Lexington', 'Fayette',
@@ -60,67 +61,62 @@ function GenerateDates(){
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
+}
 
 export function GenerateShipments(){
 
-let shipArr = [];
-let tempObj = {}
-for (let i = 0; i < 10; i++){
-    tempObj = {};
-    let [shipped, expected, delivered] = GenerateDates();
-    
-    tempObj.dateExpected = expected;
-    tempObj.dateDelivered = delivered;
-    tempObj.dateShipped = shipped;
-    tempObj.shipmentID = `${i + 1}`;
-    tempObj.driverID = `${(i%5) + 1}`;
-    let originIndex = getRandomInt(cities.length - 1);
-    tempObj.origin = cities[originIndex];
+    let shipArr = [];
+    let tempObj = {}
+    for (let i = 0; i < 10; i++){
+        tempObj = {};
+        let [shipped, expected, delivered] = GenerateDates();
+        
+        tempObj.dateExpected = expected;
+        tempObj.dateDelivered = delivered;
+        tempObj.dateShipped = shipped;
+        tempObj.shipmentID = `${i + 1}`;
+        tempObj.driverID = `${(i%5) + 1}`;
+        let originIndex = getRandomInt(cities.length - 1);
+        tempObj.origin = cities[originIndex];
 
-    let destIndex = getRandomInt(cities.length - 1);
-    while(destIndex == originIndex){
-        destIndex = getRandomInt(cities.length - 1);
+        let destIndex = getRandomInt(cities.length - 1);
+        while(destIndex == originIndex){
+            destIndex = getRandomInt(cities.length - 1);
+        }
+        tempObj.destination = cities[destIndex];
+
+        let stops = [];
+        let randStop = 0.0;
+        while(Math.random() > randStop){
+            stops.push(cities[getRandomInt(cities.length - 1)]);
+            randStop += 0.20;
+        }
+
+        if (delivered != "n/a"){
+            stops.push(cities[destIndex]);
+        }
+
+        tempObj.currentLocation = stops[stops.length - 1];
+        tempObj.stops = stops;
+
+        // tempObj.packages = GeneratePackages(i+1, tempObj);
+        // console.log(GeneratePackages(1, tempObj));
+
+        shipArr.push(tempObj);
     }
-    tempObj.destination = cities[destIndex];
 
-    let stops = [];
-    let randStop = 0.0;
-    while(Math.random() > randStop){
-        stops.push(cities[getRandomInt(cities.length - 1)]);
-        randStop += 0.20;
-    }
-
-    if (delivered != "n/a"){
-        stops.push(cities[destIndex]);
-    }
-
-    //inspect, failed, passed
-    // if (Math.random() > 0.75){
-    //     tempObj.dangerFlag = "Inspect";
-    // }else if(0.5 <= Math.random() <= 0.75){
-    //     tempObj.dangerFlag = "Failed";
-    // }else{
-    //     tempObj.dangerFlag = "Passed"
-    // }
-
-    tempObj.currentLocation = stops[stops.length - 1];
-    tempObj.stops = stops;
-
-
-    shipArr.push(tempObj);
+    console.log(shipArr);
+    return shipArr;
 }
 
-console.log(shipArr);
-return shipArr;
-}
+//GenerateShipments();
 
 
-export function GeneratePackages(){
+export function GeneratePackages(index, Shipment){
     let tempObj = {}
     let packArr = []
-    for (let i = 0; i < Shipments.length; i++){
-        //console.log(Shipments[i]);
+    let mult = parseInt(Shipment.shipmentID) - 1;
+        //console.log(Shipment);
 
         for (let j = 0; j < 10;j++){
             tempObj = {};
@@ -135,10 +131,11 @@ export function GeneratePackages(){
 
             let originIndex = getRandomInt(cities.length - 1);
             tempObj.origin = cities[originIndex];
-            tempObj.destination = Shipments[i].destination;
-            tempObj.shipmentID = Shipments[i].shipmentID;
-            tempObj.id = `${(i*10)+j + 1}`
-            tempObj.currentLocation = Shipments[i].currentLocation;
+            tempObj.destination = Shipment.destination;
+            tempObj.shipmentID = Shipment.shipmentID;
+            tempObj.id = `${(mult*10)+j+1}`
+            tempObj.currentLocation = Shipment.currentLocation;
+            tempObj.price = getRandomInt(500000) + 10;
 
             let contentIndex = getRandomInt(packages.length - 1);
             tempObj.content = packages[contentIndex];
@@ -146,10 +143,16 @@ export function GeneratePackages(){
             tempObj.weight = `${getRandomInt(50)}kg`;
 
             packArr.push(tempObj);
+            fs.appendFile("./text.txt", `${JSON.stringify(tempObj)},\n`);
         }
-    }
+        fs.appendFile("./text.txt", `\n`);
+    
     console.log(packArr);
     return packArr;
+}
+
+for (let i = 5; i < Shipments.length; i++){
+    GeneratePackages(i+1, Shipments[i]);
 }
 
 
